16b53a1b1c5383fd5347efe4783241ab16d79921
Alex Lavallee <73203142+lavalleeale@users.noreply.github.com>
2021-02-17 15:31:53 -0800

    Initial Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,111 @@
-import React from 'react';
+import React, { FormEvent, useState } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import icon from '../assets/icon.svg';
 import './App.global.css';
+import { Card, IconButton, TextField, Button } from '@material-ui/core';
+import { Create } from '@material-ui/icons';
+import Store, { Schema } from 'electron-store';
+import { v4 as uuidv4 } from 'uuid';
+import Note from './components/note';
 
-const Hello = () => {
+const schema = {
+  notes: {
+    type: 'array',
+    default: [
+      {
+        id: '1',
+        title: 'Test',
+        content: 'good',
+      },
+    ],
+  },
+} as Schema<unknown>;
+
+const store = new Store({ schema });
+
+const Index = () => {
+  const [showCompose, setShowCompose] = useState(false);
+  const [title, setTitle] = useState('');
+  const [content, setContent] = useState('');
+  const [notes, setNotes] = useState(store.get('notes'));
+  store.onDidChange('notes', (newValue) => setNotes(newValue));
+  function deleteNote(id: string) {
+    store.set(
+      'notes',
+      (store.get('notes') as Array<Record<string, string>>).filter(
+        (note) => note.id !== id
+      )
+    );
+  }
+  function onSubmit(e: FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    store.set('notes', [
+      ...(store.get('notes') as Array<Record<string, string>>),
+      { title, content, id: uuidv4() },
+    ]);
+    setShowCompose(false);
+    setTitle('');
+    setContent('');
+  }
+  function updateNote(id: string, noteTitle: string, noteContent: string) {
+    store.set('notes', [
+      ...(store.get('notes') as Array<Record<string, string>>).filter(
+        (note) => note.id !== id
+      ),
+      { title: noteTitle, content: noteContent, id: uuidv4() },
+    ]);
+  }
+
   return (
     <div>
-      <div className="Hello">
-        <img width="200px" alt="icon" src={icon} />
-      </div>
-      <h1>electron-react-boilerplate</h1>
-      <div className="Hello">
-        <a
-          href="https://electron-react-boilerplate.js.org/"
-          target="_blank"
-          rel="noreferrer"
+      <Card style={{ padding: '10px' }}>
+        <IconButton
+          onClick={() => setShowCompose(!showCompose)}
+          style={{ float: 'right' }}
         >
-          <button type="button">
-            <span role="img" aria-label="books">
-              📚
-            </span>
-            Read our docs
-          </button>
-        </a>
-        <a
-          href="https://github.com/sponsors/electron-react-boilerplate"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <button type="button">
-            <span role="img" aria-label="books">
-              🙏
-            </span>
-            Donate
-          </button>
-        </a>
-      </div>
+          <Create />
+        </IconButton>
+        <h2>Alex&apos;s First Desktop App!</h2>
+      </Card>
+      {showCompose && (
+        <Card style={{ marginTop: '10px', padding: '10px' }}>
+          <form onSubmit={onSubmit}>
+            <TextField
+              required
+              style={{ width: '100%' }}
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
+              label="Title"
+              variant="outlined"
+              id="Title"
+            />
+            <TextField
+              required
+              style={{ marginTop: '10px', width: '100%' }}
+              value={content}
+              onChange={(e) => setContent(e.target.value)}
+              label="Content"
+              variant="outlined"
+              id="Content"
+            />
+            <Button
+              aria-label={navigator.onLine ? 'Submit' : 'No Connection'}
+              disabled={!navigator.onLine}
+              style={{ float: 'right', marginTop: '10px' }}
+              variant="outlined"
+              type="submit"
+            >
+              Submit
+            </Button>
+          </form>
+        </Card>
+      )}
+      <ul style={{ listStyleType: 'none', padding: '0px' }}>
+        {(notes as Array<Record<string, string>>).map((note) => (
+          <li key={note.id}>
+            <Note note={note} deleteNote={deleteNote} updateNote={updateNote} />
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
@@ -44,7 +114,7 @@ export default function App() {
   return (
     <Router>
       <Switch>
-        <Route path="/" component={Hello} />
+        <Route path="/" component={Index} />
       </Switch>
     </Router>
   );
---
+++ src/components/note.tsx
@@ -0,0 +1,93 @@
+import React, { FormEvent, useState } from 'react';
+import '../App.global.css';
+import { Card, IconButton, TextField, Button } from '@material-ui/core';
+import { Create, Delete, CancelScheduleSend } from '@material-ui/icons';
+import PropTypes from 'prop-types';
+
+const Note = ({
+  deleteNote,
+  note,
+  updateNote,
+}: {
+  deleteNote: (arg0: string) => void;
+  note: Record<string, string>;
+  updateNote: (arg0: string, arg1: string, arg2: string) => void;
+}) => {
+  const [title, setTitle] = useState(note.title);
+  const [content, setContent] = useState(note.content);
+
+  function onSubmit(e: FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    updateNote(note.id, title, content);
+  }
+  const [showEditor, setShowEditor] = useState(false);
+  return (
+    <Card style={{ marginTop: '10px', padding: '10px' }}>
+      {showEditor ? (
+        <form onSubmit={onSubmit}>
+          <TextField
+            required
+            style={{ width: '100%' }}
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            label="Title"
+            variant="outlined"
+            id="Title"
+          />
+          <TextField
+            required
+            style={{ marginTop: '10px', width: '100%' }}
+            value={content}
+            onChange={(e) => setContent(e.target.value)}
+            label="Content"
+            variant="outlined"
+            id="Content"
+          />
+          <Button
+            aria-label="Submit"
+            style={{ float: 'right', marginTop: '10px' }}
+            variant="outlined"
+            type="submit"
+          >
+            Update
+          </Button>
+          <IconButton
+            onClick={() => setShowEditor(false)}
+            style={{ float: 'right' }}
+          >
+            <CancelScheduleSend />
+          </IconButton>
+        </form>
+      ) : (
+        <>
+          <IconButton
+            onClick={() => deleteNote(note.id)}
+            style={{ float: 'right' }}
+          >
+            <Delete />
+          </IconButton>
+          <IconButton
+            onClick={() => setShowEditor(true)}
+            style={{ float: 'right' }}
+          >
+            <Create />
+          </IconButton>
+          <h3>{note.title}</h3>
+          <p>{note.content}</p>
+        </>
+      )}
+    </Card>
+  );
+};
+
+Note.propTypes = {
+  deleteNote: PropTypes.func.isRequired,
+  note: PropTypes.shape({
+    title: PropTypes.string,
+    id: PropTypes.string,
+    content: PropTypes.string,
+  }).isRequired,
+  updateNote: PropTypes.func.isRequired,
+};
+
+export default Note;
